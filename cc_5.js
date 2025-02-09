0eb40991846315539fa6ba2bc5161607dbcc02f9
Drew Cook <dc333@usf.edu>
2025-02-09 15:43:18 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -3,4 +3,15 @@ let object = {name: "John Doe", age: 35, email: "john.doe@email.com"}; //declari
 console.log(`Customer Name: ${object.name} \nCustomer Age: ${object.age} \nCustomer Email: ${object.email}`); //logging to console customer information using template literals, using \n to line break
 
 //Task 2: Object methods. Order details scenario
-order = {orderId: 12345, totalAmount: 150, status: "Processing"};
+let order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing", //declare the standard properties
+    displayOrder: function() { //create a function called displayOrder within object
+        console.log(`Order ID: ${this.orderId}`); //use this to log the properties we declared earlier
+        console.log(`Total Amount: ${this.totalAmount}`); //we can use "this." since it is within the object
+        console.log(`Status: ${this.status}`);
+    }
+};
+console.log(`Order info:`); //console log a title for the function, could also include this in the function itself
+order.displayOrder(); //call the function
