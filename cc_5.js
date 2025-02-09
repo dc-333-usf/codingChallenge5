5108a114bfe28f0f51a3a8b4ad6f84a295a6fe85
Drew Cook <dc333@usf.edu>
2025-02-09 14:34:58 -0500

    Added semicolon, fixed caiptalization in comments.

--- cc_5.js
+++ cc_5.js
@@ -1,3 +1,6 @@
-//Task 1: Object properties. Customer Profile Scenario
-let object = {name: "John Doe", age: 35, email: "john.doe@email.com"} //declaring customer information in this object
-console.log(`Customer Name: ${object.name} \nCustomer Age: ${object.age} \nCustomer Email: ${object.email}`) //logging to console customer information using template literals, using \n to line break
+//Task 1: Object properties. Customer profile scenario
+let object = {name: "John Doe", age: 35, email: "john.doe@email.com"}; //declaring customer information in this object
+console.log(`Customer Name: ${object.name} \nCustomer Age: ${object.age} \nCustomer Email: ${object.email}`); //logging to console customer information using template literals, using \n to line break
+
+//Task 2: Object methods. Order details scenario
+order = {orderId: 12345, totalAmount: 150, status: "Processing"};
