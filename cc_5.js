392d7fff91168dc7cb2044a941e7e7c875980ec0
Drew Cook <dc333@usf.edu>
2025-02-09 14:33:00 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,3 @@
+//Task 1: Object properties. Customer Profile Scenario
+let object = {name: "John Doe", age: 35, email: "john.doe@email.com"} //declaring customer information in this object
+console.log(`Customer Name: ${object.name} \nCustomer Age: ${object.age} \nCustomer Email: ${object.email}`) //logging to console customer information using template literals, using \n to line break
